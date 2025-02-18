771da2d27d123d55ff469c9563573a639e0a8657
afuahdodeshi <almo23mt@student.ju.se>
2025-02-18 16:25:04 +0100

    Update third.js

--- java/third.js
+++ java/third.js
@@ -1,18 +1,23 @@
+// calling all the things i want to have an effect on
 const effect = document.querySelector("#effect")
 const effectImage = document.querySelector("#effectimage")
 const closeEffect = document.querySelector("#closeeffect")
-
+// selecting the class
 document.querySelectorAll(".jseffect").forEach(fullview => {
     fullview.addEventListener("click", function () {
+        // capturing the images sources in html
         effectImage.src = this.src; 
+        // manipulating css style effect into js as an effect
         effect.style.display = "flex"; 
     });
 });
+
 
 closeEffect.addEventListener("click", function(){
     effect.style.display = "none";
 });
 
+// if statement for the closing of the image
 effect.addEventListener("click", function(event){
     if (event.target === effect){
         effect.style.display = "none";
